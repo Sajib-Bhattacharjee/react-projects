5cfedce70d2aaac46267deda73d7d401952a6725
Sajib Bhattacharjee <86997775+Sajib-Bhattacharjee@users.noreply.github.com>
2023-07-25 14:12:19 +0600

    Everything Uploaded Successfully.
    Topic: 1. Complete Project-1(A Complete Todo).

---
+++ 1.Complete Project-1(A Complete Todo)/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import "font-awesome/css/font-awesome.min.css";
+
+import React from "react";
+import Home from "./COMPONENTS/Home";
+
+const App = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default App;
